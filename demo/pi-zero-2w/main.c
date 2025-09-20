53e623ad0f20b130c7bb0fe398648b8e7f03b754
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2025-09-20 17:28:50 +0200

    Adding pi zero 2w demo

---
+++ demo/pi-zero-2w/main.c
@@ -0,0 +1,44 @@
+//=============================================================================
+/*-------------------------------- Includes ---------------------------------*/
+//=============================================================================
+#include <stdio.h>
+#include "stdlib.h"
+#include <pthread.h>
+
+#include "threads/task_blink.h"
+#include "threads/task_mqtt_mng.h"
+#include "threads/task_temperature.h"
+#include "threads/task_led.h"
+//=============================================================================
+
+//=============================================================================
+/*----------------------------------- Main ----------------------------------*/
+//=============================================================================
+//-----------------------------------------------------------------------------
+int main(int argc, char** argv){
+
+    (void)argc;
+    (void)argv;
+
+    pthread_t taskBlinkHandle;
+    pthread_t taskMqttmngHandle;
+    pthread_t taskTemperatureHandle;
+    pthread_t taskLedHandle;
+
+    pthread_create( &taskBlinkHandle, NULL, taskBlink, NULL );
+    pthread_create( &taskMqttmngHandle, NULL, taskMqttmng, NULL );
+    pthread_create( &taskTemperatureHandle, NULL, taskTemperature, NULL );
+    pthread_create( &taskLedHandle, NULL, taskLed, NULL );
+
+    pthread_join( taskBlinkHandle, NULL );
+    pthread_join( taskMqttmngHandle, NULL );
+    pthread_join( taskTemperatureHandle, NULL );
+    pthread_join( taskLedHandle, NULL );
+
+    exit( 0 );
+
+    return 0;
+}
+//-----------------------------------------------------------------------------
+//=============================================================================
+
---
+++ demo/pi-zero-2w/threads/task_led.c
@@ -0,0 +1,104 @@
+//=============================================================================
+/*-------------------------------- Includes ---------------------------------*/
+//=============================================================================
+#include "task_led.h"
+
+#include "stdio.h"
+
+#include "mqttmng.h"
+#include "mqttmngConfig.h"
+#include "loggingConfig.h"
+
+#include "mhw/pi_zero_2w/led/ledpwm.h"
+//=============================================================================
+
+//=============================================================================
+/*------------------------------- Definitions -------------------------------*/
+//=============================================================================
+#define LED_CFG_MQTT_COMP_NAME  "led223"
+#define LED_CFG_MQTT_COMP_TYPE  "led"
+#define LED_CFG_MQTT_COMP_FLAGS "ri"
+
+#define LED_CFG_MQTT_COMP_ID    MQTT_MNG_CONFIG_DEV_ID "/" LED_CFG_MQTT_COMP_NAME
+//=============================================================================
+
+//=============================================================================
+/*-------------------------------- Prototypes -------------------------------*/
+//=============================================================================
+static void taskLedInitialize(void);
+static void taskLedMqttUpdateState(MQTTContext_t *pContext, MQTTPublishInfo_t *pPublishInfo);
+static void taskLedMqttUpdateRgb(MQTTContext_t *pContext, MQTTPublishInfo_t *pPublishInfo);
+static void taskLedMqttUpdateIntensity(MQTTContext_t *pContext, MQTTPublishInfo_t *pPublishInfo);
+//=============================================================================
+
+//=============================================================================
+/*---------------------------------- Task -----------------------------------*/
+//=============================================================================
+//-----------------------------------------------------------------------------
+void* taskLed(void *param){
+
+    (void)param;
+
+    taskLedInitialize();
+
+    return NULL;
+}
+//-----------------------------------------------------------------------------
+//=============================================================================
+
+//=============================================================================
+/*---------------------------- Static functions -----------------------------*/
+//=============================================================================
+//-----------------------------------------------------------------------------
+static void taskLedInitialize(void){
+
+    if( ledpwmInitialize() < 0 ){
+        LogError(( "Error initializing PWM" ));
+        while(1);
+    }
+
+    while( mqttmngInitDone() != 0 );
+    mqttmngPublishComponent(
+        LED_CFG_MQTT_COMP_NAME,
+        LED_CFG_MQTT_COMP_TYPE,
+        LED_CFG_MQTT_COMP_FLAGS
+    );
+
+    mqttmngSubscribe(LED_CFG_MQTT_COMP_ID "/state", taskLedMqttUpdateState);
+    mqttmngSubscribe(LED_CFG_MQTT_COMP_ID "/rgb", taskLedMqttUpdateRgb);
+    mqttmngSubscribe(LED_CFG_MQTT_COMP_ID "/intensity", taskLedMqttUpdateIntensity);
+}
+//-----------------------------------------------------------------------------
+static void taskLedMqttUpdateState(MQTTContext_t *pContext, MQTTPublishInfo_t *pPublishInfo){
+    
+    ( void ) pContext;
+    uint8_t state;
+    
+    state = *( (uint8_t *) pPublishInfo->pPayload );
+
+    LogInfo( ("Invoked led state callback with state %d.", state) );
+}
+//-----------------------------------------------------------------------------
+static void taskLedMqttUpdateRgb(MQTTContext_t *pContext, MQTTPublishInfo_t *pPublishInfo){
+
+    ( void ) pContext;
+    uint8_t *p;
+    
+    p = (uint8_t *) pPublishInfo->pPayload;
+
+    LogInfo( ("Invoked led rgb callback with RGB: %d %d %d.", p[0], p[1], p[2]) );
+}
+//-----------------------------------------------------------------------------
+static void taskLedMqttUpdateIntensity(MQTTContext_t *pContext, MQTTPublishInfo_t *pPublishInfo){
+
+    ( void ) pContext;
+    uint8_t intensity;
+    
+    intensity = *( (uint8_t *) pPublishInfo->pPayload );
+
+    LogInfo( ("Invoked led intensity callback with intensity %d.", intensity) );
+
+    ledpwmSetIntensity(0, intensity, 0);
+}
+//-----------------------------------------------------------------------------
+//=============================================================================
